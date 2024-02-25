26c910114ab0c679cf499b2a0fbf646861547fa7
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-25 20:29:57 -0300

    Fix username lenght

--- src/app/(pages)/register/page.tsx
+++ src/app/(pages)/register/page.tsx
@@ -18,6 +18,7 @@ export default function Register() {
 			<h1 className="m-5 text-3xl font-semibold">Register</h1>
 			<div className="flex flex-col justify-center items-center gap-4 shadow-3xl border-2 p-5 rounded-md transition-all duration-200">
 				<Input
+					maxLength={10}
 					onChange={(e) => {
 						setUsername(e.target.value);
 					}}
