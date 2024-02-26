0465913851a791b0c67302ecfeab70bd39a79b61
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-25 22:42:12 -0300

    Update post.tsx

--- src/app/(pages)/post/post.tsx
+++ src/app/(pages)/post/post.tsx
@@ -6,7 +6,7 @@ interface PostInfo {
 
 export default function BlogPost({ title, content, author }: PostInfo) {
 	return (
-		<div className="flex flex-col justify-between w-72 h-80 overflow-y-scroll overflow-hidden shadow-3xl border-2 rounded-md">
+		<div className="flex flex-col justify-between w-72 h-80 overflow-y-auto overflow-hidden shadow-3xl border-2 rounded-md">
 			<div className="flex flex-col justify-start items-center">
 				<h1 className="m-3 text-center break-all text-2xl">{title}</h1>
 				<p className="m-5 text-wrap break-all leading-relaxed">{content}</p>
