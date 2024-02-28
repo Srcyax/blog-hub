7cbc9181960d182a993577615bb8b0f738059736
Philip <95943570+Srcyax@users.noreply.github.com>
2024-02-28 15:51:19 -0300

    Update page.tsx

--- src/app/(pages)/post/page.tsx
+++ src/app/(pages)/post/page.tsx
@@ -4,7 +4,6 @@ export default function Post() {
 	return (
 		<main className="flex flex-col justify-center items-center mt-40">
 			<h1 className="m-5 text-3xl font-semibold">Create Post</h1>
-
 			<CreatePost />
 		</main>
 	);
